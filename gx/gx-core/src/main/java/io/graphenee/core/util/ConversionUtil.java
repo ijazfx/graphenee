729e2d3d383bf1b1e8af8f55998ab525f7dcd6c6
Farrukh Ijaz <ijazfx@gmail.com>
2019-10-08 18:51:44 +0500

    Fixed margins issue for root layout

--- gx/gx-core/src/main/java/io/graphenee/core/util/ConversionUtil.java
+++ gx/gx-core/src/main/java/io/graphenee/core/util/ConversionUtil.java
@@ -55,4 +55,25 @@ public class ConversionUtil {
 		return text;
 	}
 
+	public static String truncateNumber(double floatNumber) {
+		long million = 1000000L;
+		long billion = 1000000000L;
+		long trillion = 1000000000000L;
+		long number = Math.round(floatNumber);
+		if ((number >= million) && (number < billion)) {
+			float fraction = calculateFraction(number, million);
+			return Float.toString(fraction) + "M";
+		} else if ((number >= billion) && (number < trillion)) {
+			float fraction = calculateFraction(number, billion);
+			return Float.toString(fraction) + "B";
+		}
+		return Long.toString(number);
+	}
+
+	public static float calculateFraction(long number, long divisor) {
+		long truncate = (number * 10L + (divisor / 2L)) / divisor;
+		float fraction = (float) truncate * 0.10F;
+		return fraction;
+	}
+
 }
