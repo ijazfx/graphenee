0d664e0b79a97f5b21e62db648ca70f4b4c45e33
Ahmed Mirza <ahmed.mirza994@gmail.com>
2021-11-07 16:14:01 +0500

    Registered DEvice Form and list panel added

--- gx-workshop-flow/src/main/java/io/graphenee/workshop/vaadin/FlowSetup.java
+++ gx-workshop-flow/src/main/java/io/graphenee/workshop/vaadin/FlowSetup.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 
 import io.graphenee.vaadin.flow.base.GxAbstractFlowSetup;
 import io.graphenee.vaadin.flow.base.GxMenuItem;
-import io.graphenee.vaadin.flow.base.GxRegisteredDeviceListView;
 import io.graphenee.vaadin.flow.security.GxSecurityGroupListView;
 import io.graphenee.vaadin.flow.security.GxSecurityPolicyListView;
 import io.graphenee.vaadin.flow.security.GxUserAccountListView;
@@ -38,7 +37,6 @@ public class FlowSetup extends GxAbstractFlowSetup {
         securityMenuItem.add(GxMenuItem.create("User Accounts", VaadinIcon.USER.create(), GxUserAccountListView.class));
         securityMenuItem.add(GxMenuItem.create("Security Groups", VaadinIcon.TABLE.create(), GxSecurityGroupListView.class));
         securityMenuItem.add(GxMenuItem.create("Security Policies", VaadinIcon.TABLE.create(), GxSecurityPolicyListView.class));
-        securityMenuItem.add(GxMenuItem.create("Registered Device", VaadinIcon.TABLE.create(), GxRegisteredDeviceListView.class));
         items.add(securityMenuItem);
 
         return items;
