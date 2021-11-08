bad4b595169362dd86f2bef170f855f8ffbe4240
Farrukh Ijaz <fijaz@octans.ai>
2021-11-08 14:41:10 +0500

    SMS configuration added

--- gx-workshop-flow/src/test/java/io/graphenee/workshop/LocalizerServiceTest.java
+++ gx-workshop-flow/src/test/java/io/graphenee/workshop/LocalizerServiceTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-package io.graphenee.gx.workshop.test;
+package io.graphenee.workshop;
 
 import static org.junit.Assert.assertNotEquals;
 
