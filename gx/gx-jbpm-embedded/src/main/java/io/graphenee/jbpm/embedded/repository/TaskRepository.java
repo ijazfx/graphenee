9a977845dda8358137d49105e2ef038b344f2447
Saif Idrees <sidrees.idrees@gmail.com>
2021-12-30 20:20:31 +0500

    fetch task by processId

--- gx/gx-jbpm-embedded/src/main/java/io/graphenee/jbpm/embedded/repository/TaskRepository.java
+++ gx/gx-jbpm-embedded/src/main/java/io/graphenee/jbpm/embedded/repository/TaskRepository.java
@@ -15,7 +15,7 @@ public interface TaskRepository extends JpaRepository<Task, Integer> {
 	List<Task> findAllByActualOwnerId(String ownerId);
 
 	List<Task> findAllByActualOwnerIdIn(List<String> ownerIds);
-	
+
 	Page<Task> findPageByActualOwnerId(String ownerId, Pageable pageRequest);
 
 	Page<Task> findPageByActualOwnerIdAndStatusIn(String ownerId, List<String> statuses, Pageable pageRequest);
@@ -25,23 +25,19 @@ public interface TaskRepository extends JpaRepository<Task, Integer> {
 	Page<Task> findPageByActualOwnerIdInAndStatusIn(List<String> ownerIds, List<String> statuses, Pageable pageRequest);
 
 	Page<Task> findPageByOrganizationalEntitiesIdInAndStatusIn(List<String> entityIds, List<String> statuses, Pageable pageRequest);
-	
-	@Query("select t from Task t where t.processInstanceId=:processInstanceId")
-	List<Task> findTasksByProcessId(@Param("processInstanceId")  Integer processInstanceId);
 
-	@Query("select DISTINCT t from Task t \n"
-			+ "left join t.organizationalEntities oe \n"
-			+ "where (t.actualOwnerId in (:entityIds) or oe.id in (:entityIds) )\n"
-			+ "and t.archived=0 \n"
-			+ "and t.status in (:statuses)")
-	Page<Task> findOwnedAndGroupTasks(@Param("entityIds")  List<String> entityIds,@Param("statuses") List<String> statuses, Pageable pageRequest);
-	
-	@Query("select DISTINCT t from Task t \n"
-			+ "left join t.organizationalEntities oe \n"
-			+ "where (t.actualOwnerId in (:entityIds) or oe.id in (:entityIds) )\n"
-			+ "and t.archived=0 \n"
-			+ "and t.status in (:statuses)\n"
-			+ "and (lower(t.description) like lower(:searchText) or lower(t.subject) like lower(:searchText))")
-	Page<Task> searchOwnedAndGroupTasks(@Param("entityIds")  List<String> entityIds,@Param("statuses") List<String> statuses,@Param("searchText")  String searchText, Pageable pageRequest);
+	@Query("select t from Task t where t.processInstanceId=:processInstanceId")
+	List<Task> findTasksByProcessId(@Param("processInstanceId") Integer processInstanceId);
+
+	@Query("select DISTINCT t from Task t \n" + "left join t.organizationalEntities oe \n" + "where (t.actualOwnerId in (:entityIds) or oe.id in (:entityIds) )\n"
+			+ "and t.archived=0 \n" + "and t.status in (:statuses)")
+	Page<Task> findOwnedAndGroupTasks(@Param("entityIds") List<String> entityIds, @Param("statuses") List<String> statuses, Pageable pageRequest);
+
+	@Query("select DISTINCT t from Task t \n" + "left join t.organizationalEntities oe \n" + "where (t.actualOwnerId in (:entityIds) or oe.id in (:entityIds) )\n"
+			+ "and t.archived=0 \n" + "and t.status in (:statuses)\n" + "and (lower(t.description) like lower(:searchText) or lower(t.subject) like lower(:searchText))")
+	Page<Task> searchOwnedAndGroupTasks(@Param("entityIds") List<String> entityIds, @Param("statuses") List<String> statuses, @Param("searchText") String searchText,
+			Pageable pageRequest);
+
+	List<Task> findAllByDescriptionAndProcessId(String string, String processId);
 
 }
