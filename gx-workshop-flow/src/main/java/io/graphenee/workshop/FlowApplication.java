f65cde07a66bceb9b6d5a1d1aa8a935ce353e54b
Farrukh Ijaz <ijazfx@gmail.com>
2023-11-24 03:10:19 +0500

    Successful build with Java 17 and removed Vaadin Pro components

--- gx-workshop-flow/src/main/java/io/graphenee/workshop/FlowApplication.java
+++ gx-workshop-flow/src/main/java/io/graphenee/workshop/FlowApplication.java
@@ -5,15 +5,22 @@ import java.text.SimpleDateFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import io.graphenee.core.GrapheneeCoreConfiguration;
 import io.graphenee.util.TRCalendarUtil;
 
 @SpringBootApplication
-public class FlowApplication {
+public class FlowApplication extends SpringBootServletInitializer {
 
 	@Autowired
 	GrapheneeCoreConfiguration graphenee;
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FlowApplication.class);
+    }
 
 	public static void main(String[] args) {
 		TRCalendarUtil.setCustomDateFormatter(new SimpleDateFormat("dd.MM.yyyy"));
--- gx-workshop-flow/webpack.generated.js
+++ gx-workshop-flow/webpack.generated.js
@@ -70,11 +70,11 @@ let stats;
 
 const transpile = !devMode || process.argv.find(v => v.indexOf('--transpile-es5') >= 0);
 
-const watchDogPrefix = '--watchDogPort=';
-let watchDogPort = devMode && process.argv.find(v => v.indexOf(watchDogPrefix) >= 0);
+// Open a connection with the Java dev-mode handler in order to finish
+// webpack-dev-mode when it exits or crashes.
+const watchDogPort = devMode && process.env.watchDogPort;
 let client;
 if (watchDogPort) {
-  watchDogPort = watchDogPort.substr(watchDogPrefix.length);
   const runWatchDog = () => {
     client = new require('net').Socket();
     client.setEncoding('utf8');
@@ -266,9 +266,6 @@ module.exports = {
         plugins: [
           // workaround for Safari 10 scope issue (https://bugs.webkit.org/show_bug.cgi?id=159270)
           "@babel/plugin-transform-block-scoping",
-
-          // Edge does not support spread '...' syntax in object literals (#7321)
-          "@babel/plugin-proposal-object-rest-spread"
         ],
 
         presetOptions: {
