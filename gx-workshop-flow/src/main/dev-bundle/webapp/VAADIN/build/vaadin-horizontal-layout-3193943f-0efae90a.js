0037afd857b1b7212984a64ff691679a761bcb59
Farrukh Ijaz <fijaz@octans.ai>
2025-08-13 23:47:15 +0500

    Refactored and modernized

--- gx-workshop-flow/src/main/dev-bundle/webapp/VAADIN/build/vaadin-horizontal-layout-3193943f-0efae90a.js
+++
@@ -1,1 +0,0 @@
-import{I as o}from"./indexhtml-a0c9e60d.js";const r={tagName:"vaadin-horizontal-layout",displayName:"Horizontal Layout",elements:[{selector:"vaadin-horizontal-layout",displayName:"Layout",properties:[o.backgroundColor,o.borderColor,o.borderWidth,o.borderRadius,o.padding,o.gap]}]};export{r as default};
--- gx-workshop-flow/src/main/dev-bundle/webapp/VAADIN/build/vaadin-tabsheet-dd99ed9a-0d4c6c9a.js
+++
@@ -1,1 +0,0 @@
-import{I as e}from"./indexhtml-a0c9e60d.js";const r={tagName:"vaadin-tabsheet",displayName:"Tab Sheet",elements:[{selector:"vaadin-tabsheet",displayName:"TabSheet",properties:[e.padding,e.backgroundColor,e.borderWidth,e.borderColor,e.borderRadius]}]};export{r as default};
