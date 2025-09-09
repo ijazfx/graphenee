2ed192a54a334c9a7a97e6271925160aeec06122
Nabeel <mnabeel1@octans.ai>
2025-09-09 12:51:44 +0500

    fix created file

--- gx/gx-core/src/main/java/io/graphenee/od/OwnCloudFileStorage.java
+++ gx/gx-core/src/main/java/io/graphenee/od/OwnCloudFileStorage.java
@@ -29,7 +29,6 @@ import org.apache.jackrabbit.webdav.security.SecurityConstants;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Base64;
@@ -136,7 +135,8 @@ public class OwnCloudFileStorage implements FileStorage {
     public Future<FileMetaData> save(String folder, String fileName, InputStream inputStream) throws SaveFailedException {
         return Executors.newVirtualThreadPerTaskExecutor().submit(() -> {
             this.createDirectory(folder);
-            String resourcePath = this.resourcePath(folder, fileName);
+            String regex = "[^a-zA-Z0-9.]";
+            String resourcePath = this.resourcePath(folder, fileName.replaceAll(regex, ""));
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             byte[] buffer = new byte[4096];
             int bytesRead;
